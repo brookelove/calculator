44f549fdc9c78764b094e262d65c061d0aff5815
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-24 15:28:21 -0700

    creating add, subtract and multiply functions with two numbers

--- arethmatic.py
+++ arethmatic.py
@@ -1,2 +1,20 @@
 
 # include add, subtract,  multiply & divide in this area
+
+
+def adding(num1, num2):
+    # adding together
+    num3 = num1 + num2
+    return num3
+
+
+def subtracting(num1, num2):
+    # subtracting
+    num3 = num1 - num2
+    return num3
+
+
+def multiplying(num1, num2):
+    # multiplying
+    num3 = num1*num2
+    return num3
