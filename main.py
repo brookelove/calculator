a30a4ff98bdd8644f26e933c5f17d9eafbe96cec
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-26 12:45:32 -0700

    removing pi import

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # need to import the arethmatic functions
-from cmath import pi
 from arethmatic import add, divide, subtract, multiply
 # need first ask for a persons input
 print("What would you like to calculate today?\nSubmit the number of the corresponding mathmatic calculation.\n1. Add\n2. Subtract\n3. Multiply\n4. Divide")
