fb68639676a89627b2ceaedb46df22ed85bebf19
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-26 12:44:44 -0700

    fixing problems and creating calculator

--- arethmatic.py
+++ arethmatic.py
@@ -3,33 +3,29 @@
 
 def add(num1, num2):
     # adding together
-    num3 = num1 + num2
-    return num3
+    return num1 + num2
 
 
 def subtract(num1, num2):
     # subtracting
-    num3 = num1 - num2
-    return num3
+    return num1 - num2
 
 
 def multiply(num1, num2):
     # multiplying
-    num3 = num1*num2
-    return num3
+    return num1*num2
 
 
 def divide(num1, num2):
     # dividing
-    num3 = num1 / num2
-    return num3
+    return num1 / num2
 
 
-print("adding 3 and 2:")
-print(add(3, 2))
-print("subtracting 3 and 2:")
-print(subtract(3, 2))
-print("multiplying 3 and 2:")
-print(multiply(3, 2))
-print("dividing 3 and 2:")
-print(divide(3, 2))
+# print("adding 3 and 2:")
+# print(add(3, 2))
+# print("subtracting 3 and 2:")
+# print(subtract(3, 2))
+# print("multiplying 3 and 2:")
+# print(multiply(3, 2))
+# print("dividing 3 and 2:")
+# print(divide(3, 2))
--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 # need to import the arethmatic functions
+from cmath import pi
 from arethmatic import add, divide, subtract, multiply
 # need first ask for a persons input
 print("What would you like to calculate today?\nSubmit the number of the corresponding mathmatic calculation.\n1. Add\n2. Subtract\n3. Multiply\n4. Divide")
 userResponse = input()
 # have to give a response of what the person wants then ask them for the two numbers that they want to calculate
 # have to have conditional statements for what they want to do
-if (userResponse == 1):
+if userResponse == '1':
     # this is adding
-elif (userResponse == 2):
-    # this is subtracing
-elif (userResponse == 3):
+    print("You want to add two numbers together")
+    num1 = input("What's the first number: ")
+    num2 = input("What's the second number: ")
+    result = add(int(num1), int(num2))
+    print("The resulting number is", str(result))
+elif userResponse == '2':
+    # this is subtracting
+    print("You want to subtract two numbers together")
+    num1 = input("What's the first number: ")
+    num2 = input("What's the second number: ")
+    result = subtract(int(num1), int(num2))
+    print("The resulting number is", str(result))
+elif userResponse == '3':
     # this is multiplying
+    print("You want to multiply two numbers together")
+    num1 = input("What's the first number: ")
+    num2 = input("What's the second number: ")
+    result = multiply(int(num1), int(num2))
+    print("The resulting number is", str(result))
 else:
     # this is dividing
-
+    print("You want to divide two numbers together")
+    num1 = input("What's the first number: ")
+    num2 = input("What's the second number: ")
+    result = divide(int(num1), int(num2))
+    print("The resulting number is", str(result))
     # have two numbers that are num 1 and num 2
 
     # print the num 1 used and the num 2
