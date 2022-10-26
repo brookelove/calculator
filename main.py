de6100930f4f2a1064d8f8961b73ec391aa890b3
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-26 11:58:21 -0700

    planning out calculator

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from arethmatic import add, divide, subtract, multiply
 print("What would you like to calculate today?\nSubmit the number of the corresponding mathmatic calculation.\n1. Add\n2. Subtract\n3. Multiply\n4. Divide")
 userResponse = input()
 # have to give a response of what the person wants then ask them for the two numbers that they want to calculate
-# have two numbers that are num 1 and num 2
+# have to have conditional statements for what they want to do
+if (userResponse == 1):
+    # this is adding
+elif (userResponse == 2):
+    # this is subtracing
+elif (userResponse == 3):
+    # this is multiplying
+else:
+    # this is dividing
 
-# print the num 1 used and the num 2
+    # have two numbers that are num 1 and num 2
+
+    # print the num 1 used and the num 2
