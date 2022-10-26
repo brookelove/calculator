21f22d7ec8a56ed3cdcaa083fb15bf976205354b
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-26 11:31:45 -0700

    creating new file and importing math

--- arethmatic.py
+++ arethmatic.py
@@ -1,20 +1,35 @@
 
 # include add, subtract,  multiply & divide in this area
 
-
-def adding(num1, num2):
+def add(num1, num2):
     # adding together
     num3 = num1 + num2
     return num3
 
 
-def subtracting(num1, num2):
+def subtract(num1, num2):
     # subtracting
     num3 = num1 - num2
     return num3
 
 
-def multiplying(num1, num2):
+def multiply(num1, num2):
     # multiplying
     num3 = num1*num2
     return num3
+
+
+def divide(num1, num2):
+    # dividing
+    num3 = num1 / num2
+    return num3
+
+
+print("adding 3 and 2:")
+print(add(3, 2))
+print("subtracting 3 and 2:")
+print(subtract(3, 2))
+print("multiplying 3 and 2:")
+print(multiply(3, 2))
+print("dividing 3 and 2:")
+print(divide(3, 2))
---
+++ main.py
@@ -0,0 +1,5 @@
+# need to import the arethmatic functions
+from arethmatic import add, divide, subtract, multiply
+# need first ask for a persons input
+# have two numbers that are num 1 and num 2
+# print the num 1 used and the num 2
