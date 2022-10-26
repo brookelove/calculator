18cbd7988047c72a722539eccf8deafb7eeef6dc
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-26 11:40:56 -0700

    creating beginning code ofthe main py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # need to import the arethmatic functions
 from arethmatic import add, divide, subtract, multiply
 # need first ask for a persons input
+print("What would you like to calculate today?\nSubmit the number of the corresponding mathmatic calculation.\n1. Add\n2. Subtract\n3. Multiply\n4. Divide")
+userResponse = input()
+# have to give a response of what the person wants then ask them for the two numbers that they want to calculate
 # have two numbers that are num 1 and num 2
+
 # print the num 1 used and the num 2
